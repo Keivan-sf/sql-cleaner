d01f746991db1ab8e7cccfa6dc1deb2945d296b6
Keivan <keyvan0082@gmail.com>
2022-03-28 21:39:37 +0430

    Create `greet` method

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
-"use strict";
-console.log("Hello there!");
+import chalk from "chalk";
+import boxen from "boxen";
+import fs from "fs";
+const details = JSON.parse(fs.readFileSync("./package.json").toString());
+const $ = console.log;
+const greet = () => {
+    const welcomeMessage = chalk.green("Welcome!") +
+        " " +
+        chalk.red(`sql-cleaner v${details === null || details === void 0 ? void 0 : details.version}`);
+    const welcomeBox = boxen(welcomeMessage, {
+        padding: 1,
+        borderColor: "blue",
+        borderStyle: "round",
+    });
+    $(welcomeBox);
+};
+greet();
--- index.ts
+++ index.ts
@@ -1,2 +1,24 @@
 #!/usr/bin/env node
-console.log("Hello there!")
+import chalk from "chalk";
+import boxen from "boxen";
+import fs from "fs";
+const $ = console.log;
+
+const details = JSON.parse(fs.readFileSync("./package.json").toString());
+
+const greet = () => {
+    const welcomeMessage =
+        chalk.green("Welcome!") +
+        " " +
+        chalk.red(`sql-cleaner v${details?.version}`);
+
+    const welcomeBox = boxen(welcomeMessage, {
+        padding: 1,
+        borderColor: "blue",
+        borderStyle: "round",
+    });
+
+    $(welcomeBox);
+};
+
+greet();
